0b510fed9c194991a1cfb7464e88aa007cba0b0f
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-10-28 16:17:07 +0900

    Spring MVC Pattern Board v1.0

--- src/main/java/com/javaGG/springMVCBoard/dao/BDao.java
+++ src/main/java/com/javaGG/springMVCBoard/dao/BDao.java
@@ -1,25 +1,143 @@
 package com.javaGG.springMVCBoard.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.spi.DirStateFactory.Result;
+import javax.sql.DataSource;
 
 import com.javaGG.springMVCBoard.dto.BDto;
 
-public class BDao {
+public class BDao {	
 	
+	DataSource dataSource;
+	
+	public BDao() {
+		// super();
+		// TODO Auto-generated constructor stub
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public ArrayList<BDto> list(){
-		ArrayList<BDto> dtos = new ArrayList<BDto>
+		
+		ArrayList<BDto> dtos = new ArrayList<BDto>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			String query = "select * from smvc_board order by bGroup desc, bStep";
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				int bId = rs.getInt("bId");
+				String bName = rs.getString("bName");
+				String bTitle = rs.getString("bTitle");
+				String bContent = rs.getString("bContent");
+				Timestamp bDate = rs.getTimestamp("bDate");
+				int bHit = rs.getInt("bHit");
+				int bGroup = rs.getInt("bGroup");
+				int bStep = rs.getInt("bStep");
+				int bIndent = rs.getInt("bIndent");
+				
+				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return dtos;
 	}
 	
 	public void write(String bName, String bTitle, String bContent) {
 		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = dataSource.getConnection();
+			String query="insert into smvc_board (bId, bName, bTitle, bContent, bHit, bGroup, bStep, bIndent) values (smvc_board_seq.nextval, ?, ?, ?, 0, smvc_board_seq.currval, 0, 0 )";
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, bName);
+			pstmt.setString(2, bTitle);
+			pstmt.setString(3, bContent);
+			
+			int rn = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+				try {
+					if(pstmt != null) pstmt.cancel();
+					if(conn != null) conn.close();
+				} catch (Exception e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				}
+		}
 	}
 	
 	public BDto contentView(String strID) {
-		
-		BDto dto = new BDto();
-		return dto;
-	}
+        
+        BDto dto = new BDto();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        try {
+        conn = dataSource.getConnection();
+        String query = "select * from smvc_board where bId = ?";
+        pstmt = conn.prepareStatement(query);
+        rs = (ResultSet) pstmt.executeQuery();
+        
+        while (rs.next()) {
+           int bId = rs.getInt("bId");
+           String bName = rs.getString("bName");
+           String bTitle = rs.getString("bTitle");
+           String bContent = rs.getString("bContent");
+           Timestamp bDate = rs.getTimestamp("bDate");
+           int bHit = rs.getInt("bHit");
+           int bGroup = rs.getInt("bGroup");
+           int bStep = rs.getInt("bStep");
+           int bIndent = rs.getInt("bIndent");
+           
+           dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);           
+        }      
+        
+     } catch (SQLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+     } finally {
+           try {
+              if(rs != null) rs.close(); 
+              if(pstmt != null) pstmt.close();
+                 if(conn != null) conn.close();
+              } catch (Exception e2) {
+                 // TODO Auto-generated catch block
+                 e2.printStackTrace();
+              }
+     }
+        return dto;        
+     }
 	
 	public void modify(String bId, String bName, String bTitle, String bContent) {
 		
@@ -34,7 +152,7 @@ public class BDao {
 		return dto;
 	}
 	
-public void reply(String bId, String bName, String bTitle, String bContent, String bGroup, String bStep, String bIndent) {
+	public void reply(String bId, String bName, String bTitle, String bContent, String bGroup, String bStep, String bIndent) {
 		
 		
 	}
--- src/main/webapp/WEB-INF/views/content_view.jsp
+++ src/main/webapp/WEB-INF/views/content_view.jsp
@@ -7,6 +7,40 @@
 <title>content_view.jsp</title>
 </head>
 <body>
-
+	<h3>자유게시판 글 보기</h3>
+	<table width="500" cellpadding="0" cellspacing="0" border="1">
+		<form action="modify">
+         <tr>
+            <td>번 호</td>
+            <td>${content_view.bId }</td>
+         </tr>
+         <tr>
+            <td>조회수</td>
+            <td>${content_view.bHit }</td>
+         </tr>
+         <tr>
+            <td>이 름</td>
+            <td><input type="text" name=bName size="65" value="${content_view.bName }"></td>
+         </tr>
+         <tr>
+            <td>제 목</td>
+            <td><input type="text" name=bTitle size="65" value="${content_view.bTitle }"></td>
+         </tr>            
+         <tr>
+            <td align="center" colspan="2">내 용</td>
+         </tr>
+            <td align="center" colspan="2"><textarea name="bContent" rows="10" cols="65">${content_view.bContent }</textarea></td>
+         <tr>         
+            <td colspan="2"><a href="list">글 목록 보기</a>&nbsp;&nbsp;
+            <a href="reply_view?bId=${content_view.bId }">답 변</a> &nbsp;&nbsp; <a href="delete?bId=${content_view.bId }">삭 제</a>
+            </td>
+         </tr>
+         <tr>
+            <td align="center" colspan="2"><input type="submit" value="수정"></td>
+         </tr>         
+      </form>
+	
+	</table>
+	
 </body>
 </html>
