c1d968834e3ddb03acbe447a3dbbdccf1af53253
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-10-28 12:47:40 +0900

    Spring MVC Pattern Board Project v1.0

---
+++ src/main/java/com/javaGG/springMVCBoard/dao/BDao.java
@@ -0,0 +1,42 @@
+package com.javaGG.springMVCBoard.dao;
+
+import java.util.ArrayList;
+
+import com.javaGG.springMVCBoard.dto.BDto;
+
+public class BDao {
+	
+	public ArrayList<BDto> list(){
+		ArrayList<BDto> dtos = new ArrayList<BDto>
+		return dtos;
+	}
+	
+	public void write(String bName, String bTitle, String bContent) {
+		
+	}
+	
+	public BDto contentView(String strID) {
+		
+		BDto dto = new BDto();
+		return dto;
+	}
+	
+	public void modify(String bId, String bName, String bTitle, String bContent) {
+		
+	}
+	
+	public void delete(String bId) {
+		
+	}
+	
+	public BDto reply_View(String strId) {
+		
+		return dto;
+	}
+	
+public void reply(String bId, String bName, String bTitle, String bContent, String bGroup, String bStep, String bIndent) {
+		
+		
+	}
+
+}
